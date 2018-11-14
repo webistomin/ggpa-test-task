1f654af0d56198e6cccaa31f72c6e2395648744b
webistomin <webistomin@gmail.com>
2018-11-14 14:37:41 +0300

    Адатировал под планшет

--- src/js/main-nav.js
+++ src/js/main-nav.js
@@ -1,14 +1,21 @@
-var navToggle = document.querySelector('.page-header__toggle');
-var navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.page-header__toggle');
+const navMain = document.querySelector('.main-nav');
+const navLinks = document.querySelectorAll('.main-nav__link');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
-    navToggle.classList.add('page-header__toggle--opened')
+    navToggle.classList.add('page-header__toggle--opened');
+    for (let i = 0; i < navLinks.length; i++) {
+      navLinks[i].tabIndex = 0;
+    }
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
     navToggle.classList.remove('page-header__toggle--opened');
+    for (let i = 0; i < navLinks.length; i++) {
+      navLinks[i].tabIndex = -1;
+    }
   }
 });
